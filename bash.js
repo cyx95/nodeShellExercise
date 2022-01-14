706af76ceb6c278e22104dae753dfb7a627e2ba4
Diana Viglucci <dvigl86@gmail.com>
2022-01-14 16:09:39 -0500

    implemented ls

--- bash.js
+++ bash.js
@@ -1,1 +1,17 @@
-const pwd = require('./pwd')
+const pwd = require("./pwd");
+const ls = require("./ls");
+
+// pwd()
+// ls()
+
+process.stdout.write("prompt > ");
+
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+  if (cmd === "pwd") {
+    pwd();
+    process.stdout.write("\nprompt > ");
+  } else if (cmd === "ls") {
+    ls();
+  }
+});
--- pwd.js
+++ pwd.js
@@ -1,11 +1,3 @@
-process.stdout.write('prompt > ');
-
-const pwdFilePath = process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim();
-    if (cmd === 'pwd') {
-        process.stdout.write(process.cwd());
-    }
-    process.stdout.write('\nprompt > ');
-})
-
-module.exports = {pwdFilePath};
+module.exports = function() {
+    process.stdout.write(process.cwd());
+};
